0cc37112bc531a079f6511e56ad3c9fcd9968b46
Tebello Rankuatsana <justranku@gmail.com>
2026-03-30 14:25:32 +0200

    changing the loaded fxml

--- src/main/java/ranks/one/HelloApplication.java
+++ src/main/java/ranks/one/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Testing.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tt_logistics_login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 741, 471);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

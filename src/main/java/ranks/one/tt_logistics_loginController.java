f2ac00ed08f19f06e84ddb85e8932f37cedb303c
Tebello Rankuatsana <justranku@gmail.com>
2026-03-31 19:06:49 +0200

    controller for the login page

---
+++ src/main/java/ranks/one/tt_logistics_loginController.java
@@ -0,0 +1,21 @@
+package ranks.one;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class tt_logistics_loginController {
+
+    @FXML
+    private PasswordField PsdPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}

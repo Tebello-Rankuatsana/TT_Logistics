c162ef3abc6d23c713a4458a64cc4dd774208786
Tebello Rankuatsana <justranku@gmail.com>
2026-04-05 22:55:51 +0200

    adding the menu controller

---
+++ src/main/java/ranks/one/TtLogisticsMenu.java
@@ -0,0 +1,41 @@
+package ranks.one;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TtLogisticsMenu {
+
+    @FXML
+    private Button btnDriver;
+
+    @FXML
+    private Button btnReports;
+
+    @FXML
+    private Button btnTrip;
+
+    @FXML
+    private Button btnVehicle;
+
+    @FXML
+    void driverManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tripManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vehicleManagement(ActionEvent event) {
+
+    }
+
+}
